89a3547f8771432edfeec7fbd3bd2c80a44a5b7d
Python <diegorscorpion@gmail.com>
2016-02-20 16:34:31 -0600

    No sirve con 2 infrarrojos :(

--- Librerias/HTInfraredSeeker/HTInfraredSeeker.cpp
+++ Librerias/HTInfraredSeeker/HTInfraredSeeker.cpp
@@ -17,6 +17,16 @@ void InfraredSeeker::Initialize() //Se inicia la comunicacion de I2C
 
 }
 
+void InfraredSeeker2::Initialize2() //Se inicia la comunicacion de I2C
+{
+
+	Wire.begin();
+  	Wire.beginTransmission(InfraredSeeker2::Address2);
+  	Wire.write(0x12);
+  	Wire.endTransmission();
+
+}
+
 void InfraredSeeker::ReadValues(byte OffsetAddress, byte* buffer) //Se obtiene la direccion de la pelota
 {
 
@@ -38,10 +48,38 @@ void InfraredSeeker::ReadValues(byte OffsetAddress, byte* buffer) //Se obtiene l
 
 }
 
+void InfraredSeeker2::ReadValues2(byte OffsetAddress2, byte* buffer) //Se obtiene la direccion de la pelota
+{
+
+	InfraredSeeker2::Initialize2();
+	Wire.beginTransmission(InfraredSeeker2::Address2);
+	Wire.write(OffsetAddress2);
+	Wire.endTransmission();
+	Wire.requestFrom(InfraredSeeker2::Address2, 6);
+	for(byte i = 0; i < 6; i++) //arreglo
+	{
+		
+		buffer[i] = Wire.read(); 
+	
+	}
+	
+	while(Wire.available() > 0) 
+	Wire.read();
+	Wire.endTransmission();
+
+}
+
 void InfraredSeeker::ReadACRaw(byte* buffer) //Se leen los valores de el metodo anterior
 {
 	
 	ReadValues(0x49, buffer);
+
+}
+
+void InfraredSeeker2::ReadACRaw2(byte* buffer) //Se leen los valores de el metodo anterior
+{
+	
+	ReadValues2(0x49, buffer);
 
 }
 
@@ -80,11 +118,55 @@ InfraredInput InfraredSeeker::PopulateValues(byte* buffer) //Se obtienen los dat
 
 }
 
+InfraredInput2 InfraredSeeker2::PopulateValues2(byte* buffer) //Se obtienen los datos de direccion e intensidad de la pelota
+{
+
+	InfraredInput2 Data;
+	Data.Direction2 = buffer[0];
+	if(buffer[0] != 0)
+	{
+
+		if(buffer[0] % 2 == 0)
+		{
+
+			Data.Strength2 = (buffer[buffer[0] / 2] + buffer[buffer[0] / 2 + 1]) / 2;
+
+		}
+
+		else
+		{
+
+			Data.Strength2 = buffer[buffer[0] / 2 + 1];
+
+		}
+
+	}
+
+	else
+	{
+
+		Data.Strength2 = 0;
+
+	}
+
+	return Data;
+
+}
+
 InfraredInput InfraredSeeker::ReadAC() //Se leen todos los datos anteriores y se resumen en un solo grupo
 {
 
 	byte buffer[6];
 	ReadACRaw(buffer);
 	return PopulateValues(buffer);
+
+}
+
+InfraredInput2 InfraredSeeker2::ReadAC2() //Se leen todos los datos anteriores y se resumen en un solo grupo
+{
+
+	byte buffer[6];
+	ReadACRaw2(buffer);
+	return PopulateValues2(buffer);
 
 }
--- Librerias/HTInfraredSeeker/HTInfraredSeeker.h
+++ Librerias/HTInfraredSeeker/HTInfraredSeeker.h
@@ -9,6 +9,14 @@
 	  	byte Strength;
 	
 	};
+
+	struct InfraredInput2 //Sirve como un arreglo para guardar
+	{
+	
+	  	byte Direction2;
+	  	byte Strength2;
+	
+	};
 	
 	class InfraredSeeker
 	{
@@ -17,7 +25,7 @@
 		
 			static void Initialize(); //Metodo que inicia la comunicacion con el I2C
 			static void ReadACRaw(byte* buffer); //Metodo para leer valores
-			static InfraredInput ReadAC(); //Recibe todos los metodos de lectura
+			static InfraredInput ReadAC(); //Recibe todos los metodos de lectura	
 	  	
 	  	private:
 		
@@ -25,5 +33,22 @@
 			static void ReadValues(byte OffsetAddress, byte* buffer);
 			static const int Address = 0x10 / 2; //Divide by two as 8bit-I2C address is provided
 	
+	};
+
+	class InfraredSeeker2
+	{
+
+		public:
+
+			static void Initialize2(); //Metodo que inicia la comunicacion con el I2C
+			static void ReadACRaw2(byte* buffer); //Metodo para leer valores
+			static InfraredInput2 ReadAC2(); //Recibe todos los metodos de lectura
+
+		private:
+
+			static InfraredInput2 PopulateValues2(byte* buffer); //Obtiene la direccion de la pelota y la intensidad
+			static void ReadValues2(byte OffsetAddress2, byte* buffer);
+			static const int Address2 = 0x10 / 2; //Divide by two as 8bit-I2C address is provided
+
 	};
 #endif
