2c970bcac256b8d95c61e8bbf099a0ae8c92a2c9
Python <diegorscorpion@gmail.com>
2016-03-05 17:54:41 -0600

    "sensor de luz" agregado

--- Preliminares/Preliminar_complejo/Preliminar_complejo.ino
+++ Preliminares/Preliminar_complejo/Preliminar_complejo.ino
@@ -12,26 +12,54 @@ Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 
 byte ir, intensidad;
 int c_actual, c_inicio, brujula;
+int LDR1_1, LDR1_2, LDR1_3, LDRValue1_1, LDRValue1_2, LDRValue1_3;
+int LDR2_1, LDR2_2, LDR2_3, LDRValue2_1, LDRValue2_2, LDRValue2_3;
+int LDR3_1, LDR3_2, LDR3_3, LDRValue3_1, LDRValue3_2, LDRValue3_3;
+int LDR4_1, LDR4_2, LDR4_3, LDRValue4_1, LDRValue4_2, LDRValue4_3;
+int LDRValue1, LDRValue2, LDRValue3;
 
 void setup()
 {
   
   Serial.begin(9600);
   Wire.begin();
+  
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(14, OUTPUT);
+  
   robot.setM1puente1(6,7,8,9);
   robot.setM2puente1(2,17,16,15);
   robot.setM2puente2(3,14,4,5);
   robot.setM1puente1(10,11,12,13);
   
+  encenderLeds();
+  
   robot.encenderPuente1();
   robot.encenderPuente2();
+  
   c_inicio = obtenerGrados();
   
 }
 
 void loop()
 {
-    
+  
+  leerDatos();
+  
+  LDRValue1 = promedio1();
+  LDRValue2 = promedio2();
+  LDRValue3 = promedio3();
+  
   c_actual = obtenerGrados();
   brujula = arreglarCompas();
   
@@ -39,18 +67,79 @@ void loop()
   
   ir = seekerInput.Direction;
   intensidad = seekerInput.Strength;
-
-
   
   Serial.print("Infra: ");
   Serial.println(ir);
   Serial.print(" compas: ");
-  Serial.println(brujula);
+  Serial.print(brujula);
+  Serial.print(" luz 1: ");
+  Serial.print(LDRValue1);
+  Serial.print(" luz 2: ");
+  Serial.print(LDRValue2);
+  Serial.print(" luz 3: ");
+  Serial.println(LDRValue3);
   //followball();
 
 }
 
-float obtenerGrados()
+void leerDatos()
+{
+
+  LDRValue1_1 = analogRead(LDR1_1);
+  LDRValue1_2 = analogRead(LDR1_2);
+  LDRValue1_3 = analogRead(LDR1_3);
+  LDRValue2_1 = analogRead(LDR2_1);
+  LDRValue2_2 = analogRead(LDR2_2);
+  LDRValue2_3 = analogRead(LDR2_3);
+  LDRValue3_1 = analogRead(LDR3_1);
+  LDRValue3_2 = analogRead(LDR3_2);
+  LDRValue3_3 = analogRead(LDR3_3);
+  LDRValue4_1 = analogRead(LDR4_1);
+  LDRValue4_2 = analogRead(LDR4_2);
+  LDRValue4_3 = analogRead(LDR4_3);
+
+}
+
+void encenderLeds()
+{
+
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+  digitalWrite(14, HIGH);
+
+}
+
+int promedio1()
+{
+
+  return (LDRValue1_1, LDRValue1_2, LDRValue1_3)/3;
+
+}
+
+int promedio2()
+{
+
+  return (LDRValue2_1, LDRValue2_2, LDRValue2_3)/3;
+
+}
+
+int promedio3()
+{
+
+  return (LDRValue3_1, LDRValue3_2, LDRValue3_3)/3;
+
+}
+
+int obtenerGrados()
 {
   
   sensors_event_t event;
